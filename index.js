69406586fb88a936943b66e3cebf2f9970218d81
Mark <palokid@gmail.com>
2020-11-28 16:55:13 -0600

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ fs.readdir("./commands/", (err, files) => {
     bot.commands.set(props.help.name, props);
   });
   console.groupEnd();
-});
+}); 
 
 // ================================
 //  1 - Start-up & Status
